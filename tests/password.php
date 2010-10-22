c4ed7cf5392d8a4d21cc777c70b8bfc5db958011
Rowan Lewis <rowan@pixelcarnage.com>
2010-10-22 10:18:39 +1000

    Added a second password test.

--- tests/password.php
+++ tests/password.php
@@ -9,4 +9,11 @@
 	
 	var_dump($password->score(), $password->log());
 	
+	$password = new Password('F12aC9');
+	$password->checkLength();
+	$password->checkCharacters();
+	$password->checkWords();
+	
+	var_dump($password->score(), $password->log());
+	
 ?>
